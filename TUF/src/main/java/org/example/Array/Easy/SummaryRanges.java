e21a3c0008ec324d96931e09e805a46817ee9af4
ProjectKVAS1989 <projectk.vas.1989@gmail.com>
2025-12-24 23:25:35 +0530

    2025-12-23 : Problems related to Array-Easy

---
+++ TUF/src/main/java/org/example/Array/Easy/SummaryRanges.java
@@ -0,0 +1,57 @@
+package org.example.Array.Easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+    "a->b" if a != b
+    "a" if a == b
+
+Input: nums = [0,1,2,4,5,7]
+Output: ["0->2","4->5","7"]
+Explanation: The ranges are:
+[0,2] --> "0->2"
+[4,5] --> "4->5"
+[7,7] --> "7"
+
+ */
+public class SummaryRanges {
+    public static void main(String[] args) {
+        SummaryRangesSolution solution = new SummaryRangesSolution();
+        System.out.println(solution.summaryRanges(new int[]{0,2,3,4,6,8,9}));
+    }
+}
+
+class SummaryRangesSolution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> res = new ArrayList<>();
+        if(nums.length == 0) return res;
+        int curr = nums[0], currIndex = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i-1]+1 == nums[i])
+                continue;
+            else{
+                if(i-currIndex == 1) {
+                    res.add(String.valueOf(nums[currIndex]));
+                } else{
+                    res.add(curr +"->"+ nums[i - 1]);
+                }
+                curr = nums[i];
+                currIndex = i;
+            }
+        }
+        if( currIndex == nums.length -1)
+            res.add(String.valueOf(nums[currIndex]));
+        else
+            res.add(curr +"->"+ nums[nums.length - 1]);
+        return res;
+    }
+}
